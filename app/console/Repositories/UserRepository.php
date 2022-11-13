18f2e556d81dc0b6e8000fb9ad9e702cf6e754a3
Jesus Piñero <101824979+JMPS1984@users.noreply.github.com>
2022-11-13 17:25:55 -0400

    Add files via upload

---
+++ app/console/Repositories/UserRepository.php
@@ -0,0 +1,238 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Traits\Base64ToFile;
+use App\Interfaces\UserInterface;
+use App\Models\SchoolClass;
+use App\Models\Section;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use App\Repositories\PromotionRepository;
+use App\Repositories\StudentParentInfoRepository;
+use App\Repositories\StudentAcademicInfoRepository;
+
+class UserRepository implements UserInterface {
+    use Base64ToFile;
+
+    /**
+     * @param mixed $request
+     * @return string
+    */
+    public function createTeacher($request) {
+        try {
+            DB::transaction(function () use ($request) {
+                $user = User::create([
+                    'cedula'      => $request['cedula'],
+                    'first_name'    => $request['first_name'],
+                    'last_name'     => $request['last_name'],
+                    'address'       => $request['address'],
+                    'phone'         => $request['phone'],             
+                    'city'          => $request['city'],
+                    'zip'           => $request['zip'],
+                    'gender'        => $request['gender'],
+                    'nationality'   => $request['nationality'],
+                    'photo'         => (!empty($request['photo']))?$this->convert($request['photo']):null,
+                    'role'          => 'teacher',
+                    'email'         => $request['email'],
+                    'password'      => Hash::make($request['password']),
+                ]);
+                $user->givePermissionTo(
+                    'create exams',
+                    'view exams',
+                    'create exams rule',
+                    'view exams rule',
+                    'edit exams rule',
+                    'delete exams rule',
+                    'take attendances',
+                    'view attendances',
+                    'create assignments',
+                    'view assignments',
+                    'save marks',
+                    'view users',
+                    'view routines',
+                    'view syllabi',
+                    'view events',
+                    'view notices',
+                );
+            });
+        } catch (\Exception $e) {
+            throw new \Exception('Algo ha fallado al tratar de guardar los datos. '.$e->getMessage());
+        }
+    }
+
+    /**
+     * @param mixed $request
+     * @return string
+    */
+    public function createStudent($request) {
+        try {
+            DB::transaction(function () use ($request) {
+                $student = User::create([
+                    'cedula'      => $request['cedula'],
+                    'first_name'    => $request['first_name'],
+                    'last_name'     => $request['last_name'],
+                    'email'         => $request['email'],
+                    'gender'        => $request['gender'],
+                    'nationality'   => $request['nationality'],
+                    'phone'         => $request['phone'],
+                    'address'       => $request['address'],            
+                    'city'          => $request['city'],
+                    'zip'           => $request['zip'],
+                    'photo'         => (!empty($request['photo']))?$this->convert($request['photo']):null,
+                    'birthday'      => $request['birthday'],
+                    'blood_type'    => $request['blood_type'],
+                    'role'          => 'student',
+                    'password'      => Hash::make($request['password']),
+                ]);
+
+                // Store Parents' information
+                $studentParentInfoRepository = new StudentParentInfoRepository();
+                $studentParentInfoRepository->store($request, $student->id);
+
+                // Store Academic information
+                $studentAcademicInfoRepository = new StudentAcademicInfoRepository();
+                $studentAcademicInfoRepository->store($request, $student->id);
+
+                // Assign student to a Class and a Section
+                $promotionRepository = new PromotionRepository();
+                $promotionRepository->assignClassSection($request, $student->id);
+
+                $student->givePermissionTo(
+                    'view attendances',
+                    'view assignments',
+                    'submit assignments',
+                    'view exams',
+                    'view marks',
+                    'view users',
+                    'view routines',
+                    'view syllabi',
+                    'view events',
+                    'view notices',
+                );
+            });
+        } catch (\Exception $e) {
+            throw new \Exception('lgo fallo al crear los datos del estudiante. '.$e->getMessage());
+        }
+    }
+
+    public function updateStudent($request) {
+        try {
+            DB::transaction(function () use ($request) {
+                User::where('id', $request['student_id'])->update([
+
+                    'cedula'      => $request['cedula'],
+                    'first_name'    => $request['first_name'],
+                    'last_name'     => $request['last_name'],
+                    'email'         => $request['email'],
+                    'gender'        => $request['gender'],
+                    'nationality'   => $request['nationality'],
+                    'phone'         => $request['phone'],
+                    'address'       => $request['address'],
+                    'city'          => $request['city'],
+                    'zip'           => $request['zip'],
+                    'birthday'      => $request['birthday'],
+                    'blood_type'    => $request['blood_type'],
+                ]);
+
+                // Update Parents' information
+                $studentParentInfoRepository = new StudentParentInfoRepository();
+                $studentParentInfoRepository->update($request, $request['student_id']);
+
+                // Update Student's ID card number
+                $promotionRepository = new PromotionRepository();
+                $promotionRepository->update($request, $request['student_id']);
+            });
+        } catch (\Exception $e) {
+            throw new \Exception('Algo falló al tratar de actualizar la información. '.$e->getMessage());
+        }
+    }
+
+    public function updateTeacher($request) {
+        try {
+            DB::transaction(function () use ($request) {
+                User::where('id', $request['teacher_id'])->update([
+                    'cedula'      => $request['cedula'],
+                    'first_name'    => $request['first_name'],
+                    'last_name'     => $request['last_name'],
+                    'email'         => $request['email'],
+                    'gender'        => $request['gender'],
+                    'nationality'   => $request['nationality'],
+                    'phone'         => $request['phone'],
+                    'address'       => $request['address'],
+                    'city'          => $request['city'],
+                    'zip'           => $request['zip'],
+                ]);
+            });
+        } catch (\Exception $e) {
+            throw new \Exception('Algo falló al intentar aplicar la actualización. '.$e->getMessage());
+        }
+    }
+
+    public function getAllStudents($session_id, $class_id, $section_id) {
+        if($class_id == 0 || $section_id == 0) {
+            $schoolClass = SchoolClass::where('session_id', $session_id)
+                                    ->first();
+            $section = Section::where('session_id', $session_id)
+                                    ->first();
+            if($schoolClass == null || $section == null){
+                throw new \Exception('There is no class and section');
+            } else {
+                $class_id = $schoolClass->id;
+                $section_id = $section->id;
+            }
+            
+        }
+        try {
+            $promotionRepository = new PromotionRepository();
+            return $promotionRepository->getAll($session_id, $class_id, $section_id);
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to get all Students. '.$e->getMessage());
+        }
+    }
+
+    public function getAllStudentsBySession($session_id) {
+        $promotionRepository = new PromotionRepository();
+        return $promotionRepository->getAllStudentsBySession($session_id);
+    }
+
+    public function getAllStudentsBySessionCount($session_id) {
+        $promotionRepository = new PromotionRepository();
+        return $promotionRepository->getAllStudentsBySessionCount($session_id);
+    }
+
+    public function findStudent($id) {
+        try {
+            return User::with('parent_info', 'academic_info')->where('id', $id)->first();
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to get Student. '.$e->getMessage());
+        }
+    }
+
+    public function findTeacher($id) {
+        try {
+            return User::where('id', $id)->where('role', 'teacher')->first();
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to get Teacher. '.$e->getMessage());
+        }
+    }
+
+    public function getAllTeachers() {
+        try {
+            return User::where('role', 'teacher')->get();
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to get all Teachers. '.$e->getMessage());
+        }
+    }
+
+    public function changePassword($new_password) {
+        try {
+            return User::where('id', auth()->user()->id)->update([
+                'password'  => Hash::make($new_password)
+            ]);
+        } catch (\Exception $e) {
+            throw new \Exception('Failed to change password. '.$e->getMessage());
+        }
+    }
+}
